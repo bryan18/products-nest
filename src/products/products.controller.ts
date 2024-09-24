58e31b15f88d9a6c364aa44bcd0a0783662bbafa
Bryan Azofeifa Agüero <bryan18is@hotmail.com>
2024-09-24 15:37:46 -0600

    FindOne and create products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   findAll() {
     return 'This action return all the products';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} product`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+    // return `This action creates a coffee`;
   }
 }
